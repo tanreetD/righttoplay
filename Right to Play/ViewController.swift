dd37796a2a3c4f21dd2bd3eb6c25c6b637b5edbf
Tanreet Dhaliwal <tdhali@uwo.ca>
2020-03-05 09:34:56 -0500

    Initial Commit

---
+++ Right to Play/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Right to Play
+//
+//  Created by Tanreet on 2020-03-05.
+//  Copyright © 2020 Tanreet Dhaliwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
